5f849959bcb60da63db92cf322e3762f58e7b384
Amirtha M Lalas <Amirtha@NTP-LAP-746.chn.nestgroup.net>
2022-11-29 14:35:35 +0530

    added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ViewPhotoComponent } from './view-photo/view-photo.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const myroute:Routes=[
+  {
+    path:"",
+    component:ViewPhotoComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    ViewPhotoComponent
+    ViewPhotoComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myroute)
   ],
   providers: [],
   bootstrap: [AppComponent]
